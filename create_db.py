e59dd83569b58202a4e78b78a0a45fa3b51710b4
SheetalJade2019 <sheetal.jade93@gmail.com>
2022-11-05 08:58:20 +0530

    first commit

---
+++ create_db.py
@@ -0,0 +1,14 @@
+# importing required libraries
+import mysql.connector
+# download & install mysql database add here user & password 
+dataBase = mysql.connector.connect(
+  host ="localhost",
+  user ="root",
+  passwd ="root"
+)
+ 
+# preparing a cursor object
+cursorObject = dataBase.cursor()
+ 
+# creating database
+cursorObject.execute("CREATE DATABASE synkrama")
