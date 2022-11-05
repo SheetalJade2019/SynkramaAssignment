87a397ec236f0aa09bd67c0e294be288b6979c73
SheetalJade2019 <sheetal.jade93@gmail.com>
2022-11-05 10:49:27 +0530

    git ignore modified

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    User_ID = models.AutoField(primary_key=True)
+    address = models.CharField(max_length=250, default='Yes')
+    phone = models.CharField(blank=True,null=True,max_length=20)
+    # standard
--- app/urls.py
+++ app/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from .views import *
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("",include("app.urls"))
+    path("home",home, name="home"),
 ]
